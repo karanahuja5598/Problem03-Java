98f700149ce9396d63913c90f880307306d1704c
kburd <kalebmburd@gmail.com>
2020-09-22 17:22:33 -0400

    init

---
+++ src/main/java/cd/get/ready/algorithms/ArrayRotator.java
@@ -0,0 +1,11 @@
+package cd.get.ready.algorithms;
+
+public class ArrayRotator {
+
+    public int[] rotateLeft(int[] arr, int n){
+
+        return arr;
+    }
+
+
+}
---
+++ src/test/java/cd/get/ready/algorithms/ArrayRotatorTest.java
@@ -0,0 +1,31 @@
+package cd.get.ready.algorithms;
+
+import org.junit.Assert;
+
+public class ArrayRotatorTest {
+
+    public void testOne(){
+        ArrayRotator rotator = new ArrayRotator();
+        int rotateAmount = 4;
+        int[] inputArray = {1, 2, 3, 4, 5};
+
+        int[] actual = rotator.rotateLeft(inputArray, rotateAmount);
+        int[] expected = {5, 1, 2, 3, 4};
+
+        Assert.assertArrayEquals(expected, actual);
+
+    }
+
+    public void testTwo(){
+        ArrayRotator rotator = new ArrayRotator();
+        int rotateAmount = 10;
+        int[] inputArray = {41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51};
+
+        int[] actual = rotator.rotateLeft(inputArray, rotateAmount);
+        int[] expected = {77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 7};
+
+        Assert.assertArrayEquals(expected, actual);
+
+    }
+
+}
