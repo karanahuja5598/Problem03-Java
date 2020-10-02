0abfbd34b822ef2a5b974cde697fd28d8fa01015
Karan Ahuja <karanahuja5598@gmail.com>
2020-10-01 20:28:40 -0500

    Fixed code from feedback

--- src/main/java/cd/get/ready/algorithms/ArrayRotator.java
+++ src/main/java/cd/get/ready/algorithms/ArrayRotator.java
@@ -1,15 +1,14 @@
 package cd.get.ready.algorithms;
 
+import java.util.Arrays;
+
 public class ArrayRotator {
 
     public int[] rotateLeft(int[] arr, int n){
         for(int i = 0; i < n; i++) {
-            int temp = arr[0];
-            int j;
-            for(j = 0; j < arr.length - 1; j++) {
-                arr[j] = arr[j + 1];
-            }
-            arr[j] = temp;
+            int[] newArray = Arrays.copyOfRange(arr,1,arr.length + 1);
+            newArray[arr.length - 1] = arr[0];
+            arr = newArray;
         }
 
         return arr;
